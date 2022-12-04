9f21bf7e3d273516d60f81dc2e33b39e361c221f
Braulio Chavez Martinez <balochavezmtz99@hotmail.com>
2022-12-04 13:20:56 -0600

    Gps Feature Added

--- ProyectoSII.Android/MainActivity.cs
+++ ProyectoSII.Android/MainActivity.cs
@@ -6,17 +6,41 @@ using Android.Runtime;
 using Android.OS;
 using Xamarin.Forms;
 using ProyectoSII.Views;
+using Android;
 
 namespace ProyectoSII.Droid
 {
     [Activity(Label = "ProyectoSII", Icon = "@drawable/itpnlogo", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        const int RequestLocationId = 0;
+        readonly string[] LocationPermissions =
+        {
+            Manifest.Permission.AccessCoarseLocation,
+            Manifest.Permission.AccessFineLocation,
+        };
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            if((int)Build.VERSION.SdkInt >= 23)
+            {
+                if(CheckSelfPermission(Manifest.Permission.AccessFineLocation) == Permission.Granted)
+                {
+                    RequestPermissions(LocationPermissions, RequestLocationId);
+                }
+                else
+                {
+                    //Already Granted
+                }
+            }
+        }
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.FormsMaps.Init(this, savedInstanceState);
             LoadApplication(new App());
 
             MessagingCenter.Subscribe<Inscripciones>(this, "SetLandscape", sender =>
@@ -30,7 +54,21 @@ namespace ProyectoSII.Droid
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            if(requestCode == RequestLocationId) 
+            {
+                if((grantResults.Length == 1) && grantResults[0] == (int)Permission.Granted)
+                {
+                    //Permissions granted
+                }
+                else
+                {
+                    //Permissions denied
+                }
+            }
+            else
+            {
+                Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
---
+++ ProyectoSII/Views/TecMap.xaml.cs
@@ -0,0 +1,43 @@
+﻿using ProyectoSII.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoSII.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TecMap : ContentPage
+    {
+        public TecMap()
+        {
+            InitializeComponent();
+            Device.InvokeOnMainThreadAsync(() =>
+            {
+                LoadLocation();
+            });
+        }
+
+        private async void LoadLocation()
+        {
+            var location = await Geolocation.GetLastKnownLocationAsync();
+            if(location == null) { 
+                location = await Geolocation.GetLocationAsync(new GeolocationRequest()
+                {
+                    DesiredAccuracy = GeolocationAccuracy.Medium,
+                    Timeout = TimeSpan.FromSeconds(25)
+                });
+            }
+
+            if(location != null)
+            {
+                myMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude), Distance.FromMeters(100)));
+            }
+        }
+    }
+}
