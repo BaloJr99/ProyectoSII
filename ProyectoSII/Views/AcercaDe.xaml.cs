c5230b036adcf96e49f0c26df356f18a0034774b
Braulio Chavez Martinez <balochavezmtz99@hotmail.com>
2022-12-09 01:55:37 -0600

    AboutMe Added

---
+++ ProyectoSII/Views/AcercaDe.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoSII.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AcercaDe : ContentPage
+    {
+        public AcercaDe()
+        {
+            InitializeComponent();
+            Task.Run(AnimateBackGround);
+        }
+
+        private async void AnimateBackGround()
+        {
+            Action<double> forward = input => bdGradient.AnchorY = input;
+            Action<double> backward = input => bdGradient.AnchorY = input;
+
+            while (true)
+            {
+                bdGradient.Animate(name: "forward", callback: forward, start: 0, end: 1, length: 5000, easing: Easing.SinIn);
+                await Task.Delay(5000);
+                bdGradient.Animate(name: "backward", callback: backward, start: 1, end: 0, length: 5000, easing: Easing.SinIn);
+                await Task.Delay(5000);
+            }
+        }
+    }
+}
